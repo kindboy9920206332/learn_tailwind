a467a757327526732534c83fd6cc4159585f081d
kindboy <kindboy2025@gmail.com>
2024-12-10 19:40:45 +0330

    footer_poroje

---
+++ src/app/Footer/Footer_component/Footer_components.jsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+export default function Footer_components() {
+  return (
+    <>
+      <div className="  w-[400px] h-[500px] bg-slate-400 flex justify-center items-center rounded-[5px] shadow-2xl shadow-cyan-500/50  ">
+        <p className="text-red-700 font-bold text-[40px]">
+          mohammadreza haghverdy
+        </p>
+        <button className="ring-blue-500 ring-4 rounded-[5px] ">
+          click me!
+        </button>
+      </div>
+    </>
+  );
+}
---
+++ src/app/Footer/footer.jsx
@@ -0,0 +1,9 @@
+import Footer_components from "@/app/Footer/Footer_component/Footer_components";
+import Image from "next/image";
+export default function Footer() {
+  return (
+    <>
+      <Footer_components></Footer_components>
+    </>
+  );
+}
